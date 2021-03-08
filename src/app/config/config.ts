4ae3453862a5b7671d78558547580bb954da63a6
Jose Ramirez <desarrolloSoaint@soaint.com>
2021-03-08 16:46:37 -0400

    Cambios de url

--- src/app/config/config.ts
+++ src/app/config/config.ts
@@ -1,13 +1,13 @@
 // tslint:disable-next-line: eofline
 
 //URLs Local
-export const BASE_URL = 'http://localhost:8080/api/v1/';
-export const Url_Back = 'http://localhost:8080/api/v1/';
+//export const BASE_URL = 'http://localhost:8080/api/v1/';
+//export const Url_Back = 'http://localhost:8080/api/v1/';
 
 //Urls del backend SONIAT
 //export const BASE_URL = 'https://soniatback.herokuapp.com/api/v1';
 //export const Url_Back = 'https://soaint-sa-chatbot.herokuapp.com/api/v1/';
 
 //Urls del backend ISSFA
-//export const BASE_URL = 'https://issfa-chatbot.herokuapp.com/api/v1';
-//export const Url_Back = 'https://issfa-chatbot.herokuapp.com/api/v1/';
+export const BASE_URL = 'https://issfa-chatbot.herokuapp.com/api/v1';
+export const Url_Back = 'https://issfa-chatbot.herokuapp.com/api/v1/';
